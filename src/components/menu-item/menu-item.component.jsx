d9609eff6687fb7bd2bfa37724c11d1fa8d75119
ifuxas <rokaszilinskas09@gmail.com>
2022-02-16 17:16:33 +0200

    updating our app with a shop page, collection preview component and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
+  <div 
+    className={`${size} menu-item`}>
     <div 
       className='background-image' 
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
     ></div>
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
+      <div className='content'>        
+        <Link to={`${linkUrl}`}>
+          <h1 className='title'>{title.toUpperCase()}</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </Link>
+      </div>
   </div>
 )
 
